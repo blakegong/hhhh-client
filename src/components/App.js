31f838048bf20af4645c33b187852c7e7f0f8bed
Ivor, Xinzi Zhou <zhouxz@garena.com>
2016-11-05 04:11:13 +0800

    aaa

--- src/components/App.js
+++ src/components/App.js
@@ -3,22 +3,27 @@
  */
 
 import React from 'react';
+import AccountBar from './widgets/AccountBar';
+import LogoBar from './widgets/LogoBar';
+
+import bg1 from '../images/bg_1.jpg';
+
+let styles = {
+  container: {
+    width: "100%",
+    height: "100%",
+    backgroundImage: `url(${bg1})`,
+    backgroundSize: 'cover',
+  }
+};
 
 export default function({ account, children, goTo, ...others }) {
   return (
-    <div>
-      <div className="container" style={{width: 970, marginTop: 70, marginBottom: 20}}>
-        <div>
-          <a onClick={() => goTo('/')}>Home</a>
-          {account['email'] ? (
-            <a onClick={() => goTo('/a/account/')}>Account</a>
-          ) : (
-            <a href="/api/v1/google_login/">Log In</a>
-          )}
-        </div>
-        <div>
-          {children}
-        </div>
+    <div style={styles.container}>
+      <LogoBar goTo={goTo} />
+      <AccountBar account={account} goTo={goTo} />
+      <div>
+        {children}
       </div>
     </div>
   )
